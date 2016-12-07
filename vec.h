d14ffd8a4d49cc7e0b456b6342213e62e37ee222
Michael J Welsh <michael.welsh.j@gmail.com>
2016-12-07 17:23:50 -0500

    Add files via upload

--- vec.h
+++ vec.h
@@ -1,4 +1,4 @@
- /* Vec 1.1.0 -- A C library all about generic vectors.
+ /* Vec 1.0.0 -- A C library all about generic vectors.
  *
  * ABOUT:
  *      The Vec library is meant to add generic and fast vectors to C (and is compatible with C++). A
@@ -21,8 +21,7 @@
  *      enclosing scope, rather than a completely seperate scope. It may not be immediately apparent as to why
  *      macro-functions are so useful in this case, but the examples shown on github will immediately explain
  *      its practicality. If, for whatever reason, the respective function cannot carry out its task on the
- *      vec because it is out of memory (except for the 'vec_reserve(...)' and 'vec_shrink(...)'
- *      functions which do nothing in such a scenario), it will raise a segment violation.
+ *      vec because it is out of memory, it will raise a segment violation.
  *
  *      Fat pointer diagram:
  *          +--------+-------------------------------------------+
@@ -37,27 +36,30 @@
  *          - sizeof the stored item
  *
  * USAGE:
- *      This library is self contained in one single header file and can be used by simply #including the 
- *      'vec.h' file, e.g.:
+ *      This library is self contained in one single header file and can be used either in header only
+ *      mode or in implementation mode. The header only mode is used by default when included and allows
+ *      including this header in other headers and does not contain the actual implementation.
  *
+ *      The implementation mode requires to define the preprocessor macro VEC_IMPLEMENTATION in *one*
+ *      .c/.cpp file before #including this file, e.g.:
+ *
+ *          #define VEC_IMPLEMENTATION
  *          #include "vec.h"
  *
- *      Also optionally define the symbols listed in the section "OPTIONAL DEFINES" below before the 
- *      #include if you want to use additional functionality or need more control over the library. 
+ *      Also optionally define the symbols listed in the section "OPTIONAL DEFINES" below *only* in
+ *      implementation mode if you want to use additional functionality or need more control over the
+ *      library. 
  *
  * OPTIONAL DEFINES:
  *      VEC_PRIVATE
- *          If defined declares all functions as static, so they can only be accessed from within the
- *          file that is using this header.
+ *          If defined declares all functions as static, so they can only be accessed inside the file
+ *          that contains the implementation.
  *
  *      VEC_MAX_PREALLOC (<max prealloc size>)
  *          By default, the maximum preallocation size of a vec is one megabyte. 
  *      
  *      VEC_MALLOC <func name>
  *          By default, uses 'malloc' from the stdlib. 
- *		
- *		VEC_CALLOC <func name>
- *			By default, uses 'calloc' from the stdlib.
  *
  *      VEC_REALLOC <func name>
  *          By default, uses 'realloc' from the stdlib.
@@ -75,11 +77,22 @@
  *          - The reason a structure could not be used to represent the vec's header and its contents was
  *            because vec's in this library are "generic", so pointers to void are used exclusively. 
  *            Unfortunately, you can't have a flexible array member be void (e.g.: 'void foo[];').
- *      4. What standard was this library written under?
+ *      4. Why isn't 'VEC_CALLOC/calloc' used/defined?
+ *          - Because there would always have to be two seperate allocations (one for the header, and the
+ *            other for the vec's contents) if it were used, which is a no-go.
+ *      5. What standard was this library written under?
  *          - C99.
- *      5. Why do the macro functions in the Vec API use 'do { ... } while (0)' constructs?
+ *      6. Why do the macro functions in the Vec API use 'do { ... } while (0)' constructs?
  *          - It ensures the macro always behaves the same, regardless of how semicolons and curly
  *            brackets are used in invoking the code.
+ *      7. Why does the API raise 'SIGSEGV' when out of memory?
+ *          - First of all, there is no way for the user to detect whether or not the vec could be
+ *            properly allocated based on the respective function's return value. From the user's
+ *            perspective, a vec can be equal to NULL and still be valid. One may argue that the
+ *            functions which don't create new vec's, but rather add to a vec already created, should
+ *            just do nothing to the vec and return silently in such a scenario. However, this would give 
+ *            way to a memory condition (like a race condition) in which more than likely the outcome of 
+ *            the user's program would be unintentionally different depending on the scenario.
  */
 #ifndef VEC_H_
 #define VEC_H_
@@ -119,10 +132,6 @@ extern "C" {
     #define VEC_MALLOC malloc
 #endif
 
-#ifndef VEC_CALLOC
-	#define VEC_CALLOC calloc
-#endif
-
 #ifndef VEC_REALLOC
     #define VEC_REALLOC realloc
 #endif
@@ -157,7 +166,7 @@ VEC_API     void    vec_remove(void *vec, size_t index);
         if (_vec_hdr_len(vec) == _vec_hdr_cap(vec)) {                                                           \
             size_t new_cap, new_total_size;                                                                     \
             if(_vec_hdr_cap(vec) <= VEC_MAX_PREALLOC) {                                                         \
-                new_cap = (size_t) (_vec_hdr_cap(vec) * 1.5);                                                   \
+                new_cap = _vec_hdr_cap(vec) * 2;                                                                \
                 new_total_size = sizeof(size_t) * 3 + (new_cap) * _vec_hdr_item_size(vec);                      \
             } else {                                                                                            \
                 new_cap = _vec_hdr_cap(vec) + 1;                                                                \
@@ -183,7 +192,7 @@ VEC_API     void    vec_remove(void *vec, size_t index);
             if (_vec_hdr_len(vec) == _vec_hdr_cap(vec)) {                                                       \
                 size_t new_cap, new_total_size;                                                                 \
                 if(_vec_hdr_cap(vec) <= VEC_MAX_PREALLOC) {                                                     \
-                    new_cap = (size_t) (_vec_hdr_cap(vec) * 1.5);                                               \
+                    new_cap = _vec_hdr_cap(vec) * 2;                                                            \
                     new_total_size = sizeof(size_t) * 3 + (new_cap) * _vec_hdr_item_size(vec);                  \
                 } else {                                                                                        \
                     new_cap = _vec_hdr_cap(vec) + 1;                                                            \
@@ -221,7 +230,7 @@ VEC_API     void    vec_remove(void *vec, size_t index);
         if ((size_t) size == size && size > _vec_hdr_cap(vec)) {                                                \
             void *temp = VEC_REALLOC(_vec_hdr_addr(vec), sizeof(size_t) * 3 + size * _vec_hdr_item_size(vec));  \
             if (temp == NULL) {                                                                                 \
-                break;																							\
+                raise(SIGSEGV);                                                                                 \
             }                                                                                                   \
             vec = _vec_cast(vec) ((char *) temp + sizeof(size_t) * 3);                                          \
                                                                                                                 \
@@ -237,7 +246,7 @@ VEC_API     void    vec_remove(void *vec, size_t index);
                 _vec_hdr_addr(vec), sizeof(size_t) * 3 + new_cap * _vec_hdr_item_size(vec)                      \
             );                                                                                                  \
             if (temp == NULL) {                                                                                 \
-                break;																							\
+                raise(SIGSEGV);                                                                                 \
             }                                                                                                   \
             vec = _vec_cast(vec) ((char *) temp + sizeof(size_t) * 3);                                          \
                                                                                                                 \
@@ -280,13 +289,16 @@ VEC_API     void    vec_remove(void *vec, size_t index);
  *
  * ========================================================
  */
+#ifdef VEC_IMPLEMENTATION
+
+
 VEC_API inline void* vec_new(size_t size) {
     return vec_newlen(size, 1);
 }
 
 
 VEC_API inline void* vec_newlen(size_t size, size_t nitems) {
-    void *vec = VEC_CALLOC(1, sizeof(size_t) * 3 + size * nitems);
+    void *vec = VEC_MALLOC(sizeof(size_t) * 3 + size * nitems);
     if (vec == NULL) {
         raise(SIGSEGV);
     }
@@ -295,6 +307,7 @@ VEC_API inline void* vec_newlen(size_t size, size_t nitems) {
     _vec_hdr_cap(vec) = nitems;
     _vec_hdr_len(vec) = 0;
     _vec_hdr_item_size(vec) = size;
+    memset(vec, 0, size * nitems);
 
     return vec;
 }
@@ -366,6 +379,9 @@ VEC_API inline void vec_remove(void *vec, size_t index) {
         --_vec_hdr_len(vec);
     }
 }
+
+
+#endif
 
 
 #endif
